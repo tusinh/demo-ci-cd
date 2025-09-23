ca853ae91bafc11aff2ec9f0054cb0e29e7f3ffc
CauTu <tuansinh111@gmail.com>
2025-09-23 22:50:40 +0700

    test CICD 3

--- test/test.js
+++ test/test.js
@@ -1,3 +1,10 @@
+import { sum } from "../src/sum.js";
+
 test('Basic test should pass', () => {
   expect(true).toBe(true);
+});
+
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
 });
