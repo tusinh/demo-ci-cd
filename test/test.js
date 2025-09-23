91b0a23b8a7765347654ad96416163811a5262a7
CauTu <tuansinh111@gmail.com>
2025-09-23 16:30:21 +0700

    second commit

--- test/test.js
+++ test/test.js
@@ -1,7 +1,3 @@
-const assert = require('assert');
-
-describe('Basic test', function() {
-  it('should return true', function() {
-    assert.strictEqual(true, true);
-  });
+test('Basic test should pass', () => {
+  expect(true).toBe(true);
 });
